a26e3ef0113c2636d9cc3c48d5f9884bae880f7a
Alexandria Davis <alexandriadavis@csumb.edu>
2019-02-05 22:39:16 -0800

    Add external navigation page thorugh php

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 
 <head>
-        <title>Title</title>
+        <title>Design Patterns</title>
         <link rel="stylesheet" type="text/css" href="css/external.css">
 </head>
 <body>
-Hello World
+        <header>
+                <h>Design Patterns</h>
+        </header>
+        <navigation>
+        <?php include navigation.php ?>
+        </navigation>
+        <content>
+        content
+        </content>
 </body>
