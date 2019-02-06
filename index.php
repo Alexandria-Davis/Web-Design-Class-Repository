2b4fbb6bf625e91832725d31c00d735057ae4ba5
Alexandria Davis <alexandriadavis@csumb.edu>
2019-02-05 22:55:24 -0800

    Added better page control

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-
+<?php 
+        if isset $_GET['page'] {
+                $page = $_GET['page']; 
+        }
+        else {
+                $page = 'main';
+        }
+?>
 <head>
         <title>Design Patterns</title>
         <link rel="stylesheet" type="text/css" href="css/external.css">
@@ -8,9 +15,10 @@
                 <h>Design Patterns</h>
         </header>
         <navigation>
-        <?php include navigation.php ?>
+                <?php include "navigation.php"; /*Display Navigation*/?>
         </navigation>
         <content>
         content
+                <?php include "${page}.php"?>
         </content>
 </body>
