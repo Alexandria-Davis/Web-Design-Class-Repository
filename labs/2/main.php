f566cc1203fbff643ba70b63aeca540f2770d03a
Alexandria Davis <alexandriadavis@csumb.edu>
2019-02-13 16:48:05 -0800

    cleaned up folder to act as general

---
+++ labs/2/main.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Guess a number</title>
+    <link type="text/css" rel="sample.css">
+    <script src="guess.java" type="text/javascript"></script>
+  </head>
+  <body>
+    
+  </body>
+</html>
