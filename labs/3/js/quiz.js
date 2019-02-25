f0ce02d4d3694a99c879d2135739dda9ab429947
Alexandria Davis <alexandriadavis@csumb.edu>
2019-02-25 18:35:41 +0000

    add points

--- labs/3/js/quiz.js
+++ labs/3/js/quiz.js
@@ -1,21 +1,24 @@
-var questions = [
+questions = [  /*global questions*/
     {
         Type: "radio",
         Question: "Of the following names, which one belongs to a character which is often bested by their own prey?",
         correctAnswer: ["Tom"],
-        incorrectAnswers: ["Jill", "Jane", "Jerry"]
+        incorrectAnswers: ["Jill", "Jane", "Jerry"],
+        Points: 1
     },
     {
         Type: "radio",
         Question: "Of the following names, which one belongs to a character which is known for defeating feline preditors using comedic antics?",
         correctAnswer: ["Jerry"],
-        incorrectAnswers: ["Jill", "Jane", "Tom", "Doe", "Daffy"]
+        incorrectAnswers: ["Jill", "Jane", "Tom", "Doe", "Daffy"],
+        Points: 1
     },
     {
         Type: "radio",
         Question: "<p>Answer this well known riddle:</p><quote><p>As I was going to St. Ives,</p><p>I met a man with seven wives,</p><p>Each wife had seven sacks,</p><p>Each sack had seven cats,</p><p>Each cat had seven kits:</p><p>Kits, cats, sacks, and wives,</p><p>How many were there going to St. Ives?</p></quote>",
         correctAnswer: ["1"],
-        incorrectAnswers: ["7", "8", "9","2402","2403"]
+        incorrectAnswers: ["7", "8", "9","2402","2403"],
+        Points: 1
     },
     {
         Type: "radio",
@@ -27,30 +30,41 @@ var questions = [
             Type: "checkbox",
         Question: "Most forms of weather can have adverse effects on human technology. Select all that are known to make it difficult to use screens.",
         correctAnswer: ["Bright and Sunny", "Rainy"],
-        incorrectAnswers: ["Overcast","All of the above", "None of the above"]
+        incorrectAnswers: ["Overcast","All of the above", "None of the above"],
+        Points: 1
     },
     {
         Type: "Radio",
         Question: "You a variable in Javascript. You set this variable to the string 'False'.",
         correctAnswer: ["The variable is Truthy"],
-        incorrectAnswers: ["The variable is Falsy"]
+        incorrectAnswers: ["The variable is Falsy"],
+        Points: 1
     }
     ];
-
-
 var submitButton = document.querySelector("#submit");
-submitButton.addEventListener("click",function(){submits(questions)});
+submitButton.addEventListener("click",function(){submits()});
 generate_quiz(questions);
 
 function generate_quiz(questions) {
-    var form = document.querySelector("quiz");
+    var quiz_area = document.querySelector("quiz");
+    var form = document.createElement("table");
+    quiz_area.appendChild(form);
     for (var i = 0; i < questions.length; i++)
     {   
+        var row = document.createElement("tr");
+        var collabel = document.createElement("td");
+        var colquestion = document.createElement("td")
+        row.appendChild(collabel)
+        row.appendChild(colquestion)
         var qlabel = document.createElement("qlabel");
         var qnumber = document.createElement("qnumber");
         qnumber.innerHTML = i;
         qlabel.appendChild(qnumber);
-        form.appendChild(qlabel);
+        var points_label = document.createElement("point_score")
+        points_label.innerHTML = ` out of ${questions[i].Points}`
+        points_label.setAttribute("id",`points_for_${i}`)
+        qlabel.appendChild(points_label)
+        collabel.appendChild(qlabel);
         
         var question = document.createElement("question");
         question.setAttribute("qustionNumber",i);
@@ -78,7 +92,8 @@ function generate_quiz(questions) {
             answer.appendChild(answer_label);
             question.appendChild(answer);
         }
-        form.appendChild(question);
+        colquestion.appendChild(question);
+        quiz_area.appendChild(row)
     }
 }
 function shuffleArray(array) {
@@ -89,15 +104,25 @@ function shuffleArray(array) {
         [array[i], array[j]] = [array[j], array[i]];
     }
 }
-function submits(questions) {
-    
+function submits() {
+    var points = 0;
+    var max_points = 0;
+    var qpoints;
     for (var i = 0; i < questions.length; i++)
     {
+        max_points+= questions[i].points;
+        qpoints = 0;
         var temp = $("input[name='question"+i+"']:checked");
-        for (i = 0; i < temp.length; i++) {
-            var selected = console.log(temp[i].id);
+        for (var j = 0; j < temp.length; j++) {
+            var selected = temp[j].id;
             var selection = document.querySelector("label[for='"+selected+"']").innerHTML;
             console.log(selection);
+            if(questions[i].correctAnswer.includes(selection))
+            {
+                qpoints+= questions[i].Points/questions[i].correctAnswer.length;
+            }
+            document.getElementById(`points_for_${i}`).innerHTML = `${qpoints} out of ${questions[i].Points}`
+            points += qpoints
         }
         
         //var form = document.querySelector("quiz");
