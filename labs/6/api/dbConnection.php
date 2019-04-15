4f8c8276a3bfa8220c537cbc4ae91b9510fc3a4f
Alexandria Davis <alexandriadavis@csumb.edu>
2019-04-15 21:44:47 +0000

    Add Enviornment Awareness

--- labs/6/api/dbConnection.php
+++ labs/6/api/dbConnection.php
@@ -1,11 +1,12 @@
 <?php
 function getDatabaseConnection($dbname = 'ottermart')
 {
-    $host='localhost';
-    $username='alexandriadavis';
-    $password = '';
+    $database_name = $_ENV["db_name"]?:$dbname;
+    $host= $_ENV['db_host']?:'localhost';
+    $username=$_ENV["db_user"]?:'alexandriadavis';
+    $password = $_ENV["db_pass"]?:'';
     
-    $dbConn = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
+    $dbConn = new PDO("mysql:host=$host;dbname=$database_name",$username,$password);
     
     $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
