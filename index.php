4d8174baf209053a4b718760b7b9f0d76d8e5dd9
Alexandria Davis <alexandriadavis@csumb.edu>
2019-04-15 21:20:00 +0000

    add index

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        <h2>Homeworks</h2>
+        <ul>
+            <li>
+                <a href="/labs/6">Ottermart</a>
+            </li>
+            
+        </ul>
+        <h2>Labs</h2>
+        <ul>
+            <li>
+                <a href="/labs/1">Lab 1</a>
+            </li>
+            <li>
+                <a href="/labs/2">Lab 2</a>
+            </li>
+            <li>
+                <a href="/labs/3">Lab 3</a>
+            </li>
+            <li>
+                <a href="/labs/4">Lab 4</a>
+            </li>
+            <li>
+                <a href="/labs/5">Lab 5</a>
+            </li>
+            <li>
+                <a href="/labs/6">Ottermart</a>
+            </li>
+            
+        </ul>
+        <h2>Midterm</h2>
+        <ul>
+            <li>
+                <a href="/midterm">Midterm</a>
+            </li>
+            
+        </ul>
+        
+    </body>
+</html>
